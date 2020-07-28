fe1bd383c0c44468c640beb53ae2f454d69279dd
Lucky Shrestha <luckshrestha@gmail.com>
2020-07-28 23:28:50 +0545

    Updates

---
+++ precache-manifest.d1416a2c6eac5d5e79dae879776508b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7476336d6f1a9c97cb009bc341a55ab1",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "5af2b1671d0d01df6b6c",
+    "url": "/todo/static/css/main.0c06d317.chunk.css"
+  },
+  {
+    "revision": "25f96f4a720ba06f2e80",
+    "url": "/todo/static/js/2.8a9a3d1b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo/static/js/2.8a9a3d1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5af2b1671d0d01df6b6c",
+    "url": "/todo/static/js/main.e0db9ad1.chunk.js"
+  },
+  {
+    "revision": "10fd3631c43d335a3db9",
+    "url": "/todo/static/js/runtime-main.2a8ec137.js"
+  }
+]);

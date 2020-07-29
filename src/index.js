c48fd25713664840d00a6676ee90dcec277cc179
Lucky Shrestha <luckshrestha@gmail.com>
2020-07-29 14:10:15 +0545

    Add stylesheet

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import './index.scss';
+import './styles/index.scss';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
